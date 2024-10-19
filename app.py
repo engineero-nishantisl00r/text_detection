9baa660c996add5a52b08813b6b7e08caf6bc600
engineero-nishantisl00r <137928914+engineero-nishantisl00r@users.noreply.github.com>
2024-10-19 19:30:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ user_essay = st.text_area("Enter your essay:")
 if st.button("Check if AI-generated"):
     if user_essay.strip():
         is_ai_generated, probabilities = predict_essay(user_essay, model, tokenizer, threshold=0.98)
-        st.write(f"Is AI-generated: {is_ai_generated[0]}")
+        st.write(f"This Essay is AI-generated: {is_ai_generated[0]}")
     else:
         st.write("Please enter an essay.")
